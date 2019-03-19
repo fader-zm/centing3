8f742083c0951d78191adb43b3ce2f93f25f9f93
zhuxiaoqing <13546940062@163.com>
2019-03-19 11:24:33 +0800

    用户注册v1.0

--- ihome/libs/yuntongxun/sms.py
+++ ihome/libs/yuntongxun/sms.py
@@ -6,13 +6,16 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context  # 全局取消证书验证
 
 # 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
-_accountSid = '8aaf0708568d4143015697b0f4960888'
+_accountSid = '8a216da86904c06001696132ded32b98'
 
 # 说明：主账号Token，登陆云通讯网站后，可在控制台-应用中看到开发者主账号AUTH TOKEN
-_accountToken = '57c6c3ef3cef47e680519a734f6812f8'
+_accountToken = 'd15c1e78f957409f9874d143f8f7696d'
 
 # 请使用管理控制台首页的APPID或自己创建应用的APPID
-_appId = '8aaf0708568d4143015697b0f56e088f'
+_appId = '8a216da86904c06001696132df242b9e'
+
+
+
 
 # 说明：请求地址，生产环境配置成app.cloopen.com
 _serverIP = 'sandboxapp.cloopen.com'
--- ihome/modules/api/passport.py
+++ ihome/modules/api/passport.py
@@ -4,13 +4,14 @@ from flask import request, abort, current_app, jsonify, make_response, json, ses
 
 from ihome import sr, db
 from ihome.libs.captcha.pic_captcha import captcha
+from ihome.libs.yuntongxun.sms import CCP
 from ihome.models import User
 from ihome.modules.api import api_blu
 from ihome.utils import constants
 from ihome.utils.response_code import RET
 
 
-# 获取图片验证码
+# 获取图片验证码  url:"/api/v1.0/imagecode?cur=
 @api_blu.route("/imagecode")
 def get_image_code():
     """
@@ -18,13 +19,22 @@ def get_image_code():
     2. 生成图片验证码
     3. 保存编号和其对应的图片验证码内容到redis
     4. 返回验证码图片
-    :return:
     """
-    pass
+    # 1.获取传入的验证码编号，并编号是否有值
+    cur = request.args.get("cur")
+    if not cur:
+        return abort(404)
+    # 2.生成图片验证码  调用captcha.generate_captcha 返回一個元組　图片名称　验证码 图片数据
+    image_name,real_image_code,image_data = captcha.generate_captcha()
+    # 3.保存编号和其对应的图片验证码内容到redis 设置5分钟有效时长
+    sr.setex(cur,constants.IMAGE_CODE_REDIS_EXPIRES,real_image_code)
+    # 4.返回验证码图片  考虑兼容问题 再响应头设置返回数据的格式
+    response = make_response(image_data)
+    response.headers["Content_Type"]="image/png"
+    return response
 
 
-
-# 获取短信验证码
+# 获取短信验证码 post请求 url:/api/v1.0/smscode
 @api_blu.route('/smscode', methods=["POST"])
 def send_sms():
     """
@@ -35,9 +45,53 @@ def send_sms():
     5. 生成发送短信的内容并发送短信
     6. redis中保存短信验证码内容
     7. 返回发送成功的响应
-    :return:
     """
-    pass
+    #1. 接收参数(手机号码 用户填写的图片验证码 唯一编号)并判断是否有值
+    mobile = request.json.get("mobile")
+    image_code_id=request.json.get("image_code_id")
+    image_code =request.json.get("image_code")
+
+    if not all([mobile,image_code_id,image_code]):
+        return jsonify(errno=RET.PARAMERR,errmsg="参数不足")
+
+    # 2.判断手机号码格式是否正确
+    if not re.match("1[3578][0-9]{9}",mobile):
+        return jsonify(errno=RET.PARAMERR,errmsg="电话号码格式不对")
+
+    # 3.通过传入的图片唯一编码去redis中查询真实的图片验证码内容
+    try:
+        real_image_code = sr.get("image_code")
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="redis数据库查询出错")
+
+    # 4.把从redis取到的图片验证码和用户填写的验证码进行比对
+    if real_image_code:
+        sr.delete("image_code")
+    else:
+        return jsonify(errno=RET.NODATA,errmsg="图片验证码5分钟过期")
+    if real_image_code.lower() != image_code_id.lower():
+        return jsonify(errno=RET.DATAERR,errmsg="用户填写的图片验证码错误")
+    # TODO:在获取短信验证码之前 判断该号码是否已经注册过了
+
+    # 5. 验证码正确 调用CPP对象的send_template_sms发送短信验证码 3个参数手机号码 [验证码,时效],模板
+    #  生成6位数的验证码  不足6未0补齐
+    real_phone_code = random.randint(1,999999)
+    real_phone_code = "%06d" % real_phone_code
+    #  发送成功返回0  失败-1
+    try:
+        result = CCP().send_template_sms(mobile,[real_phone_code,5],1)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR,errmsg="云通讯发送短信异常")
+    if result == -1:
+        return jsonify(errno=RET.THIRDERR,errmsg="云通讯发送短信异常")
+
+    # 6. 验证码发送成功 redis中保存短信验证码内容 手机号码作为键
+    sr.setex(mobile,constants.SMS_CODE_REDIS_EXPIRES,real_phone_code)
+    # 7. 返回发送成功的响应
+    return jsonify(errno=RET.OK,errmsg="发送短信验证码成功")
+
 
 
 
