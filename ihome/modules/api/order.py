ac19ec34484e67fadced52340cd23a95b0e055bd
liyujiang <liyujiangko@126.com>
2019-03-20 17:33:01 +0800

    “订单模块-时间冲突修改”

--- ihome/modules/api/order.py
+++ ihome/modules/api/order.py
@@ -48,14 +48,14 @@ def add_order():
         if days > house.max_days:
             return jsonify(errno=RET.PARAMERR, errmsg="预定天数大于最大预订天数")
 
-     # 如果订单状态不是取消或者拒单,则该订单不可进行下单
+     # 如果订单状态是取消或者拒单,则该订单仍可进行下单
 
     try:
-        date_count = Order.query.filter(or_(and_(start_date <= Order.begin_date, end_date >= Order.begin_date ),
+        date_count = Order.query.filter(not_(Order.status.in_(["CANCELED", "REJECTED"])).filter
+                                        (or_(and_(start_date <= Order.begin_date, end_date >= Order.begin_date),
                                             and_(start_date <= Order.end_date, end_date >= Order.end_date),
                                             and_(start_date >= Order.begin_date, end_date <= Order.end_date),
-                                            and_(start_date <= Order.begin_date, end_date >= Order.end_date))
-                                    ).filter(not_(Order.status.in_(["CANCELED", "REJECTED"]))).count()
+                                            and_(start_date <= Order.begin_date, end_date >= Order.end_date)))).count()
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg="查询参数错误")
@@ -110,7 +110,7 @@ def get_orders():
         for order in order_list if order_list else []:
             order_dict_list.append(order.to_dict())
         data = {
-            "orders":order_dict_list
+            "orders": order_dict_list
         }
         return jsonify(errno=RET.OK, errmsg="查询订单信息成功", data=data)
     if role == "landlord":
